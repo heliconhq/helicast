fc8dcea0c8dacddfae17a342dd4916dd46b9bf9a
leandro-helicon <leandro.salemi@helicon.ai>
2024-04-04 17:40:40 +0200

    added compose subpackage

---
+++ helicast/compose/__init__.py
@@ -0,0 +1,8 @@
+from helicast.compose._pandas_pipeline import *
+from helicast.compose._pandas_transformed_target_regressor import *
+
+__all__ = []
+for v in dir():
+    if not v.startswith("_"):
+        __all__.append(v)
+del v
