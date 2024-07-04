9d3ebeb789ffec7821eed2483071c91dfd53b465
Leandro Salemi <leandro.salemi@helicon.ai>
2024-07-04 12:18:55 +0200

    remove pandassklearn module, use sklearn instead

--- src/helicast/pandas_sklearn/__init__.py
+++
@@ -1,9 +0,0 @@
-from helicast.pandas_sklearn._pandas_pipeline import *
-from helicast.pandas_sklearn._pandas_transformed_target_regressor import *
-from helicast.pandas_sklearn._pandas_transformer import *
-
-__all__ = []
-for v in dir():
-    if not v.startswith("_"):
-        __all__.append(v)
-del v
