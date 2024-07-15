b1777a26c76b0d6ead5c6f5401a740b463ac93c9
Leandro Salemi <leandro.salemi@helicon.ai>
2024-07-15 14:15:44 +0200

    refactor typing to avoid circular deps

--- src/helicast/typing/_timeseries_dataframe.py
+++ src/helicast/typing/_timeseries_dataframe.py
@@ -1,33 +1,60 @@
-from typing import Annotated
+from typing import Annotated, Callable, Union
 
 import pandas as pd
 from pydantic.functional_validators import AfterValidator
 
-from helicast.validation._pandas_timeseries_validation import DatetimeIndexValidator
-
 __all__ = [
     "TimeDataFrame",
     "RegularTimeDataFrame",
-    "TzAwareTimeDataFrame",
-    "TzAwareRegularTimeDataFrame",
 ]
+
+
+def _validate_time_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    if not isinstance(df.index, pd.DatetimeIndex):
+        raise ValueError("DataFrame must have a DatetimeIndex")
+    return df
+
+
+def _validate_regular_time_dataframe(
+    frequency: Union[bool, pd.Timedelta],
+) -> Callable[[pd.DataFrame], pd.DataFrame]:
+    def _validate(df: pd.DataFrame) -> pd.DataFrame:
+        df = _validate_time_dataframe(df)
+
+        if frequency is False:
+            return df
+
+        diff = (df.index[1:] - df.index[:-1]).unique()
+
+        if frequency is True:
+            if len(diff) > 1:
+                raise ValueError(
+                    f"DataFrame must have a unique frequency. "
+                    f"Found unique frequencies: {diff}."
+                )
+        elif isinstance(frequency, pd.Timedelta):
+            if len(diff) > 1:
+                raise ValueError(
+                    f"DataFrame must have a frequency of {frequency}. "
+                    f"Found unique frequencies: {diff}."
+                )
+            elif diff[0] != frequency:
+                raise ValueError(
+                    f"DataFrame must have a frequency of {frequency}. "
+                    f"Found {diff[0]} instead."
+                )
+        else:
+            raise ValueError(f"Invalid frequency: {frequency}")
+        return df
+
+    return _validate
 
 
 #: Annotated type for time-series DataFrame, that is a DataFrame with a DatetimeIndex.
 #: The index can have any frequency and/or multiple frequencies.
-TimeDataFrame = Annotated[pd.DataFrame, AfterValidator(DatetimeIndexValidator())]
+TimeDataFrame = Annotated[pd.DataFrame, AfterValidator(_validate_time_dataframe)]
 
 #: Annotated type for time-series DataFrame with a unique frequency
 RegularTimeDataFrame = Annotated[
-    pd.DataFrame, AfterValidator(DatetimeIndexValidator(frequency=True))
-]
-
-#: Similar to TimeDataFrame, but timezones are enforced to be aware.
-TzAwareTimeDataFrame = Annotated[
-    pd.DataFrame, AfterValidator(DatetimeIndexValidator(tz="aware"))
-]
-
-#: Similar to RegularTimeDataFrame, but timezones are enforced to be aware.
-TzAwareRegularTimeDataFrame = Annotated[
-    pd.DataFrame, AfterValidator(DatetimeIndexValidator(frequency=True, tz="aware"))
+    pd.DataFrame, AfterValidator(_validate_regular_time_dataframe(True))
 ]
